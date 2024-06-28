7b35b2b6ec030a6798203f4562ba8b4f835b72c1
Zaid Arif <zaid.zrf@gmail.com>
2024-06-28 19:20:39 +0530

    Made Hire Me button interactive

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+document.getElementById('download-btn').addEventListener('click', () => {
+    document.getElementById('modal').style.display = 'block';
+});
+
+document.getElementById('yes-btn').addEventListener('click', () => {
+    window.open('zaidArifResume.pdf', '_blank');
+    document.getElementById('modal').style.display = 'none';
+});
+
+document.getElementById('no-btn').addEventListener('click', () => {
+    document.getElementById('modal').style.display = 'none';
+});
